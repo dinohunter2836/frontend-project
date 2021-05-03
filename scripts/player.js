aca3b3b043eba05a9a2c1f03afc2fa0ed5cb26cb
Artur Yakovlev <artur.yakovlev1414@gmail.com>
2021-05-03 05:56:04 +0300

    Implemented player class and song queue feature

--- scripts/player.js
+++ scripts/player.js
@@ -1,37 +1,84 @@
-let player = document.getElementById("musicplayer");
-let audio_source = document.getElementById("audiosource");
-let volumeBar = document.getElementById("volume");
-let progressBar = document.getElementById("progressbar");
-let queue = [];
-let position = 0;
-player.volume = volumeBar.value / 100;
-progressBar.value = 0;
+export class Player {
 
-function play_audio(url) {
-    audio_source.src = url;
-    player.load();
-    player.play();
-}
+    constructor (queue = [], position = -1) {
+        this.audio_source = document.getElementById("audiosource");
+        this.player = document.getElementById("musicplayer");
+        this.cover = document.getElementById("song_cover");
+        this.name = document.getElementById("song_name");
+        this.artist = document.getElementById("song_artist");
+        this.progressBar = document.getElementById("progressbar");
+        this.play_button = document.getElementById("play_button");
+        this.volumeBar = document.getElementById("volume");
 
-player.ontimeupdate = () => {
-    if (!Number.isNaN(player.duration)) {
-        progressBar.value = player.currentTime / player.duration * 100;
+        this.player.volume = this.volumeBar.value / 100;
+        this.progressBar.value = 0;
+        this.queue = queue;
+        this.position = position;
+        this.playing = false;
+
+        this.player.onended = () => {
+            if (this.position < this.queue.length) {
+                this.next();
+            }
+        }
+
+        this.player.ontimeupdate = () => {
+            if (!Number.isNaN(this.player.duration)) {
+                this.progressBar.value = this.player.currentTime / this.player.duration * 100;
+            }
+        }
+        
+        this.progressBar.oninput = () => {
+            if (!Number.isNaN(this.player.duration)) {
+                this.player.currentTime = this.player.duration * this.progressBar.value / 100;
+            }
+        }
+
+        this.play_button.onclick = () => {
+            this.play();
+        }
+
+        this.volumeBar.oninput = () => {
+            player.volume = this.volumeBar.value / 100;
+        }
     }
-}
 
-progressBar.oninput = () => {
-    if (!Number.isNaN(player.duration)) {
-        player.currentTime = player.duration * progressBar.value / 100;
+    play(queue, position) {
+        console.log('1', this.queue);
+        console.log('2', this.position);
+        console.log('3', this.playing);
+        if (typeof queue === "undefined") {
+            if (!Number.isNaN(this.player.duration)) {
+                if (this.playing) {
+                    this.player.pause();
+                    this.playing = false;
+                } else {
+                    this.player.play();
+                    this.playing = true;
+                }
+            } else {
+                this.next();
+                this.playing = true;
+            }
+        } else {
+            this.queue = queue;
+            console.log('123', typeof position)
+            this.position = (typeof position === "undefined") ? 0 : position;
+            this.next();
+        }
     }
-}
 
-let play_button = document.getElementById("play_button");
-
-play_button.onclick = () => {
-    let url = "music/LinkinPark-Meteora/07 - Faint.mp3";
-    play_audio(url);
-}
-
-volumeBar.oninput = () => {
-    player.volume = volumeBar.value / 100;
+    next() {
+        let song = this.queue[this.position];
+        console.log('1', this.queue);
+        console.log('2', this.position);
+        this.audio_source.src = song.src;
+        this.cover.src = song.cover;
+        console.log(song);
+        this.name.innerHTML = song.stripped_name;
+        this.artist.innerHTML = song.artist;
+        this.position++;
+        this.player.load();
+        this.player.play();
+    }
 }
